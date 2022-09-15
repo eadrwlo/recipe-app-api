7091f2267959acad336ef8f816e44435d24297e4
adrian <adrian.wlodarczyk@bluesoft.com>
2022-09-15 23:03:14 +0200

    Add custom User class and Manager with UTs

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-i1i315xk)_#0$=b4b964yn#3a=sht9r^@z5%3yd23goqs&=p$^
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,12 +69,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = None
-print(os.environ.get('DB_USE_SQLITE'))
-if os.environ.get('DB_USE_SQLITE'):
+print("Use SQL Lite? ", os.environ.get('DB_USE_SQLITE'))
+if os.environ.get('DB_USE_SQLITE') == 'True':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -94,8 +91,6 @@ else:
             'PORT': '5432',
         }
     }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -115,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -129,7 +123,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -139,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- dcc.sh
+++ dcc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo $1
+echo "$1"
 docker compose run --rm app sh -c "$1"
